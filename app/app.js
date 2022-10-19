c4e443885e6c6c7683ccbde638d7fc9c52a3cc10
Younes Brahimi <43382860+younesbram@users.noreply.github.com>
2022-10-19 14:38:34 -0400

    Fixing some packages + some logging improvements

--- app/app.js
+++ app/app.js
@@ -38,5 +38,5 @@ app.get('/profile', requiresAuth(), (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`CDT app listening on port ${port}`)
 })
